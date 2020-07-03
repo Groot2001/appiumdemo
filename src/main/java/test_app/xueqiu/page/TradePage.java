3788920fed53c2aa42dcb6f6c1642822a50a35ed
groot2001 <youngboy71@163.com>
2020-07-03 23:00:19 +0800

    webview插播课-课后练习1

--- src/main/java/test_app/xueqiu/page/TradePage.java
+++ src/main/java/test_app/xueqiu/page/TradePage.java
@@ -6,7 +6,7 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-public class TradePage extends BasePage{
+public class TradePage extends BasePage {
     public AppiumDriver<WebElement> driver;
 
     public TradePage(AndroidDriver driver) {
@@ -15,7 +15,7 @@ public class TradePage extends BasePage{
     }
 
     @Step("A股开户")
-    public TradePage registAStock(String mobileTel){
+    public TradePage registAStock(String mobileTel) {
         /* 调试代码：找到webview上下文
         System.out.println(driver.getPageSource());
         for (int i = 0; i <3 ; i++) {
@@ -29,30 +29,28 @@ public class TradePage extends BasePage{
             }
         }*/
         driver.context(driver.getContextHandles().toArray()[1].toString()); //切换到webview上下文
-
-        /* 调试代码：找到"A股开户"入口所在的windowHandle
-        driver.getWindowHandles().stream().forEach(win->{
-            System.out.println(win);
+        //遍历找到"A股开户"入口所在的windowHandle
+        Object AStock_win = driver.getWindowHandles().stream().filter(win -> {
             driver.switchTo().window(win);
-            System.out.println(driver.getPageSource());
+            return driver.getPageSource().contains("A股开户");
+        }).toArray()[0];
+        //切换到"A股开户"入口所在的windowHandle: AStock_win
+        driver.switchTo().window(AStock_win.toString());
 
-        });*/
-        Object[] wins = driver.getWindowHandles().toArray();
-        driver.switchTo().window(wins[0].toString());  //切换到"A股开户"入口所在的windowHandle
         click(By.cssSelector(".trade_home_info_3aI")); //点击"A股开户"
-        /* 调试代码：找到进入"A股开户"页面后的windowHandle
-        driver.getWindowHandles().stream().forEach(win->{
-            System.out.println(win);
-            driver.switchTo().window(win);
-            System.out.println(driver.getPageSource());
 
-        });*/
-        Object[] wins_after = driver.getWindowHandles().toArray();
-        driver.switchTo().window(wins_after[wins_after.length-1].toString());  //切换到进入"A股开户"页面所在windowHandle
-        sendKeys(By.id("phone-number"),mobileTel);
+        //找到"A股开户"页面的windowHandle
+        Object AStock_page_win = driver.getWindowHandles().stream().filter(win -> {
+            driver.switchTo().window(win);
+            return driver.getPageSource().contains("立即开户");
+        }).toArray()[0];
+        //切换到"A股开户"页面所在windowHandle: AStock_page_win
+        driver.switchTo().window(AStock_page_win.toString());
+
+        sendKeys(By.id("phone-number"), mobileTel);
         click(By.cssSelector(".btn-code"));
         //todo: 同一个号码频繁发送验证码会触发图形验证码弹窗
-        sendKeys(By.id("code"),"000000");
+        sendKeys(By.id("code"), "000000");
         click(By.cssSelector("btn-submit"));
 
         return this;
